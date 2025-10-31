30a7f45d0fb7b8699ef878827f931603a8a11fc0
TrialBlazer23 <trialblazer212@gmail.com>
2025-10-31 17:49:14 +0000

    Fix hardcoded AGP version in build.gradle.kts to 8.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.3.0")
+        classpath("com.android.tools.build:gradle:8.6.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
         classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.22-1.0.17")
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.22")
