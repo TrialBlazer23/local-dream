8a5596c58296273d1dce5aad4bc17f2010762494
TrialBlazer23 <trialblazer212@gmail.com>
2025-10-31 17:54:17 +0000

    Update to Kotlin 2.0.0, KSP 2.0.0-1.0.21, kotlinx-serialization 1.7.3, add missing theme_mode string

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.6.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.22-1.0.17")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.22")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.0.0-1.0.21")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:2.0.0")
     }
 }
